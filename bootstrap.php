7a7d0503c26ed109126f635512c424086a7bd860
Miguel Ibarra <miguel@digitec.ws>
2016-05-08 05:19:39 +0000

    Adding env library

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,11 @@ use Silex\Application;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Acme\Provider\User as UserProvider;
 use Silex\Provider\DoctrineServiceProvider;
+use Dotenv\Dotenv;
+
+$dotenv = new Dotenv(__DIR__);
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_USER', 'DB_PASS', 'DB_DATABASE']);
 
 $app = new Application;
 
@@ -15,11 +20,10 @@ $app->register(new ServiceControllerServiceProvider);
 $app->register(new DoctrineServiceProvider, [
     'db.options' => [
         'driver' => 'pdo_mysql',
-        'host' => 'localhost',
-	'dbname' => 'messages',
-	'user' => 'root',
-	'password' => 'root',
-	
+        'host' => getenv('DB_HOST'),
+	'dbname' => getenv('DB_DATABASE'),
+	'user' => getenv('DB_USER'),
+	'password' => getenv('DB_PASS'),
     ]
 ]);
 
