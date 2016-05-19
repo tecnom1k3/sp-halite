94a6e775b53f25de5bb002d8dce0a22d1604afe7
Miguel Ibarra <a-miibarra@expedia.com>
2016-05-18 20:52:47 -0700

    documenting user provider and updating dependencies

--- src/Provider/User.php
+++ src/Provider/User.php
@@ -21,27 +21,59 @@ class User implements ControllerProviderInterface
      */
     public function connect(Application $app)
     {
+        /*
+         * define the user service.
+         *
+         * Requires the doctrine entity manager
+         */
         $app['service.user'] = $app->share(function () use ($app) {
             return new UserService($app['doctrine.entityManager']);
         });
 
+        /*
+         * define the message service.
+         *
+         * Requires the doctrine entity manager, the user service, and the halite service
+         */
         $app['service.message'] = $app->share(function () use ($app) {
             return new MessageService($app['doctrine.entityManager'], $app['service.user'], $app['service.halite']);
         });
 
+        /*
+         * define the user controller
+         *
+         * requires the user service
+         */
         $app['controller.user'] = $app->share(function () use ($app) {
             return new UserController($app['service.user']);
         });
 
+        /*
+         * define the message controller
+         *
+         * requires the message service
+         */
         $app['controller.message'] = $app->share(function () use ($app) {
             return new MessageController($app['service.message']);
         });
 
         /** @var ControllerCollection $controllers */
         $controllers = $app['controllers_factory'];
+        /*
+         * routes GET requests to /user to the getList method on user controller
+         */
         $controllers->get('/', 'controller.user:getList');
+        /*
+         * routes GET requests to /user/{userId}/messages to the getList method on the message controller
+         */
         $controllers->get('/{userId}/messages', 'controller.message:getList');
+        /*
+         * routes POST requests to /user/{userId}/messages to the save method onn the message controller
+         */
         $controllers->post('/{userId}/messages', 'controller.message:save');
+        /*
+         * routes GET requests to /users/{userId}/messages/{messageId} to the get method on the message controller
+         */
         $controllers->get('/{userId}/messages/{messageId}', 'controller.message:get');
         return $controllers;
     }
